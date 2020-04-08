cbfcbe56190f20737344aecf67165694b6b9496f
Robert <11153104+x1sec@users.noreply.github.com>
2020-04-08 15:54:13 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+// credit: https://superuser.com/revisions/1530942/3
 #include <windows.h>
 
 #define ES_CONTINUOUS       0x80000000
@@ -7,12 +8,21 @@
 int main()
 {
     int result = 0;
+    
+    // Hide from the task bar
     HWND hWnd = GetConsoleWindow();
+    
+    // Window needs to be minimised first
     ShowWindow(hWnd, SW_MINIMIZE);
     ShowWindow(hWnd, SW_HIDE);
+    
+    
     while(1)
     {
+        // credit: https://superuser.com/revisions/1530942/3
         result = SetThreadExecutionState(ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED | ES_CONTINUOUS);
+        
+        // Sleep for 20 seconds
         Sleep(20 * 1000);
     }
     return 0;
